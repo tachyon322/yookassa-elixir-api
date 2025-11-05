9fd5ccf5741fb92451203017040996ae7c1983d8
denisPcFedora <den.voprosik@gmail.com>
2025-11-05 19:57:02 +0700

    chore: bump version to 0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Yookassa.MixProject do
   def project do
     [
       app: :yookassa,
-      version: "0.1.3",
+      version: "0.1.4",
       # Recommend setting to ~> 1.14 for broader compatibility
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
